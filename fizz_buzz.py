497c386f46c8502503756e0e27893c49fce70ec4
Omar <omaraguilalira2@gmail.com>
2025-05-26 12:40:51 -0600

    implement FizzBuzz solution

---
+++ fizz_buzz.py
@@ -0,0 +1,13 @@
+limit = 1000
+counter = 1
+
+while counter <= limit:
+    if counter % 3 == 0 and counter % 5 == 0:
+        print("FizzBuzz")
+    elif counter % 3 == 0:
+        print("Fizz")
+    elif counter % 5 == 0:
+        print("Buzz")
+    else:
+        print(counter) 
+    counter += 1 
